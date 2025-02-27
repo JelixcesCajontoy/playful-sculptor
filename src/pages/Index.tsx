16263d447a72ef86ebddf4b9627c4730e3906ae2
JelixcesCajontoy <bc.jelixces.cajontoy@cvsu.edu.ph>
2025-02-27 15:46:55 +0800

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -122,9 +122,12 @@ const Index = () => {
           {/* Text content */}
           <div className="relative z-10 text-center">
             <h1 className="text-white text-5xl font-bold mb-4">
+              Cavite State University - Bacoor City Campus
+            </h1>
+            <h1 className="text-white text-5xl mb-4">
               INSTITUTIONAL DEVELOPMENT OFFICE
             </h1>
-            <h2 className="text-white text-6xl font-bold mb-4 tracking-tight">
+            <h2 className="text-white text-8xl font-bold mb-4 tracking-tight">
               ONLINE ACCREDITATION
             </h2>
           </div>
